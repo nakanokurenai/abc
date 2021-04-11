8348b6df3c2b47e146a516721e04b4bb3aa00638
otofune <otofune@otofune.me>
2021-04-12 01:00:51 +0900

    Update template

--- templates/tpl.c
+++ templates/tpl.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 int main() {
   // your code here...
+  char N[11];
+  scanf("%s", N);
 
   printf("\n");
   return 0;
